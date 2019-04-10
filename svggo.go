9ad575fe3413d8c6ba1f51b82e541ee5b376c03c
Jan Tlamicha <shadow.to.root@gmail.com>
2019-04-10 20:45:26 +0200

    Init

---
+++ svggo.go
@@ -0,0 +1,12 @@
+package svgoptimizer
+
+/*
+Text minimizer of course, but also crop invisible draws.
+*/
+
+/*
+IDEA:
+make bit map(matrix) w x h
+make every drawable for now rect, solve rotation
+mark every pixel it draw with id of drawable object
+*/
